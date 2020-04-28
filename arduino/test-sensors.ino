177563feaa4c9d9b41cae48576bbdf3ee4ee7791
juan230500 <juanpo.alvarado@gmail.com>
2020-04-28 13:54:49 -0600

    sensors and http post

---
+++ arduino/test-sensors.ino
@@ -0,0 +1,64 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
+
+#define DHTPIN 14
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() {
+ 
+  Serial.begin(9600);                                  //Serial connection
+  WiFi.begin("Alva", "Telememo");   //WiFi connection
+ 
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+ 
+    delay(1000);
+    Serial.println("Waiting for connection");
+ 
+  }
+
+  dht.begin();
+ 
+}
+ 
+void loop() {
+ 
+ if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  DynamicJsonDocument doc(1024);
+
+  doc["temperature"] = t;
+  doc["humidity"]   = h;
+  String jsonData;
+  serializeJson(doc, jsonData);
+  Serial.println(jsonData);
+ 
+   HTTPClient http;    //Declare object of class HTTPClient
+ 
+   http.begin("http://192.168.100.12:3000/users/");      //Specify request destination
+   http.addHeader("Content-Type", "application/json");  //Specify content-type header
+   http.addHeader("Authorization", "xFn5?n9f4A^hC2+#");
+ 
+   int httpCode = http.POST(jsonData);   //Send the request
+   String payload = http.getString();                //Get the response payload
+ 
+   Serial.println(httpCode);   //Print HTTP return code
+   Serial.println(payload);    //Print request response payload
+ 
+   http.end();  //Close connection
+ 
+ }else{
+ 
+    Serial.println("Error in WiFi connection");   
+ 
+ }
+ 
+  delay(10*60*1000);
+ 
+}
